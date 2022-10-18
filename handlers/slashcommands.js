9ff21bb2aaa0f539f768053538d960e4d4353613
growtoups <67888122+growtoups@users.noreply.github.com>
2022-10-18 19:11:18 -0400

    Update slashcommands.js

--- handlers/slashcommands.js
+++ handlers/slashcommands.js
@@ -1,4 +1,5 @@
 const fs = require('node:fs');
+ const chalk = require('chalk')
 const path = require('node:path');
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'scommands');
@@ -11,6 +12,23 @@ for (const file of commandFiles) {
 	if ('data' in command && 'execute' in command) {
 		client.commands.set(command.data.name, command);
 	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		console.log(chalk.cyan('[Micium-Wings] The command at ${filePath} is missing a required "data" or "execute" property.'));
 	}
 }
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(chalk.cyan('[Micium-Wings] No command matching ${interaction.commandName} was found.'));
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
