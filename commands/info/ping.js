64ea2e413225210ffd166f58295fd5ebf1354c78
growtoups <67888122+growtoups@users.noreply.github.com>
2022-10-18 20:32:58 -0400

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,16 +1,5 @@
 const Discord = require('discord.js')
-const wrapdactyl = require('wrapdactyl');
-const ptero = new wrapdactyl({
-    url: 'https://dash.micium.cloud',    // Panel url
-    client: 'ptlc_dZD0cu2I6CG3l9GiQDCU7RYVSLVxKeTJAnmZoc0ArlN',                 // Client api key
-    application: 'ptla_FKSmXiJI7SfFp4YBgx46MSXSm4of5z4f7Elgxw49qRo',            // Application api key
-    options: {                      // Options to optimise the performance
-        cache: true,                // Enable or disable cache
-        events: true,               // Enable of disable events
-        checkInterval: 5000,        // An interval which checks the status of tokens and panel
-        updateCacheInterval: 30000  // An interval which updates cache in case you have it enabled
-    }
-})
+const ptero = require('wrapdactyl');
 
 module.exports = {
     name: "ping",
