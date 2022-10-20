b4e945403dc0afd40aa53155e27d8c669544776a
growtoups <67888122+growtoups@users.noreply.github.com>
2022-10-19 22:20:02 -0400

    Update sunsuspend.js

--- scommands/admin/sunsuspend.js
+++ scommands/admin/sunsuspend.js
@@ -1,1 +1,16 @@
-
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const ID = interaction.options.getString('ServerID');
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('Unsuspend')
+		.setDescription('Unsuspend somebody server')
+		.addUserOption(option =>
+			option
+				.setName('ServerID')
+				.setDescription('Put the server id of the server you want to unsuspend.')
+				.setRequired(true))
+		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+		.setDMPermission(false),
+  await interaction.reply(`Unsuspended ${ID}`);
+};
+ptero.servers.unsuspend(ID)
